de8284afad1f853fadd369137718eae54e8cf61c
Carlos <carlostamarit18@gmail.com>
2021-02-08 12:33:40 +0100

    Modifico el apartado routing

--- GonnaGo/src/app/app-routing.module.ts
+++ GonnaGo/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
+import { FormPageComponent } from './form-page/form-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { SavedPageComponent } from './saved-page/saved-page.component';
+import { VisitedPageComponent } from './visited-page/visited-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'card', component:CardComponent},
+  {path: 'detailpage', component:DetailPageComponent},
+  {path: 'formpage', component:FormPageComponent},
+  {path: 'loginpage', component:LoginPageComponent},
+  {path: 'main', component:MainComponent},
+  {path: 'menu', component:MenuComponent},
+  {path: 'registerpage', component:RegisterPageComponent},
+  {path: 'savedpage', component:SavedPageComponent},
+  {path: 'visitedpage', component:VisitedPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
