8a0168346966ec5001e146543cad16359844dd60
IvanJRF <ivanjoseroig@gmail.com>
2021-02-09 10:17:45 +0100

    Creados los metodos add, post, put y delete en CityController

--- API/src/Controller/CityController.php
+++ API/src/Controller/CityController.php
@@ -12,14 +12,129 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CityController extends AbstractController
 {
-    /**
-     * @Route("/city", name="city")
-     */
-    public function index(): Response
+    private $cityRepository;
+
+    public function __construct(CityRepository $cityRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CityController.php',
-        ]);
+        $this->cityRepository = $cityRepository;
+    }
+
+    /**
+     * @Route("/addCity", name="addCity", methods={"POST"})
+     */
+    public function addCity(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $name = $data['name'];
+        $weather = $data['weather'];
+        $size = $data['size'];
+        $nativeLanguage = $data['nativeLanguage'];
+        $continent = $data['continent'];
+        $nativeLanguageDificulty = $data['nativeLanguageDificulty'];
+        $costOfLiving = $data['costOfLiving'];
+        $role = $data['role'];
+        $img = $data['img'];
+        $country = $data['country'];
+        $video = $data['video'];
+
+        if (empty($name) || empty($weather) || empty($size) || empty($nativeLanguage) || empty($continent) || empty($nativeLanguageDificulty) || empty($costOfLiving) || empty($role) || empty($img) || empty($country) || empty($video)) {
+            throw new NotFoundHttpException('Expecting mandatory parameters!');
+        }
+
+        $this->cityRepository->saveCity($name, $weather, $size, $nativeLanguage, $continent, $nativeLanguageDificulty, $costOfLiving, $role, $img, $country, $video);
+
+        return new JsonResponse(['status' => 'City Created'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/getCity/{idCity}", name="getCity", methods={"GET"})
+     */
+    public function getCity($idCity): JsonResponse
+    {
+        $city = $this->cityRepository->findOneBy(['idCity' => $idCity]);
+
+        $data = [
+            'idCity' => $city->getIdCity(),
+            'name' => $city->getName(),
+            'weather' => $city->getWeather(),
+            'size' => $city->getSize(),
+            'nativeLanguage' => $city->getNativeLanguage(),
+            'continent' => $city->getContinent(),
+            'nativeLanguageDificulty' => $city->getNativeLanguageDificulty(),
+            'costOfLiving' => $city->getCostOfLiving(),
+            'role' => $city->getRrole(),
+            'img' => $city->getImg(),
+            'country' => $city->getCountry(),
+            'video' => $city->getVideo()
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getCities", name="getCities", methods={"GET"})
+     */
+    public function getCities(): JsonResponse
+    {
+        $cities = $this->cityRepository->findAll();
+
+        $data = [];
+
+        foreach ($cities as $city) {
+            $data[] = [
+                'idCity' => $city->getIdCity(),
+                'name' => $city->getName(),
+                'weather' => $city->getWeather(),
+                'size' => $city->getSize(),
+                'nativeLanguage' => $city->getNativeLanguage(),
+                'continent' => $city->getContinent(),
+                'nativeLanguageDificulty' => $city->getNativeLanguageDificulty(),
+                'costOfLiving' => $city->getCostOfLiving(),
+                'role' => $city->getRrole(),
+                'img' => $city->getImg(),
+                'country' => $city->getCountry(),
+                'video' => $city->getVideo()
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/updateCity/{idCity}", name="updateCity", methods={"PUT"})
+     */
+    public function editCity(Request $request, $idCity): JsonResponse
+    {
+        $city = $this->cityRepository->findOneBy(['idCity' => $idCity]);
+        $data = json_decode($request->getContent(), true);
+
+        empty($data['name']) ? true : $city->setName($data['name']);
+        empty($data['weather']) ? true : $city->setWeather($data['weather']);
+        empty($data['size']) ? true : $city->setSize($data['size']);
+        empty($data['nativeLanguage']) ? true : $city->setNativeLanguage($data['nativeLanguage']);
+        empty($data['continent']) ? true : $city->setContinent($data['continent']);
+        empty($data['nativeLanguageDificulty']) ? true : $city->setNativeLanguageDificulty($data['nativeLanguageDificulty']);
+        empty($data['costOfLiving']) ? true : $city->setCostOfLiving($data['costOfLiving']);
+        empty($data['role']) ? true : $city->setRole($data['role']);
+        empty($data['img']) ? true : $city->setImg($data['img']);
+        empty($data['country']) ? true : $city->setCountry($data['country']);
+        empty($data['video']) ? true : $city->setVideo($data['video']);
+
+        $updatedCity = $this->cityRepository->updateCity($city);
+
+        return new JsonResponse(['status' => 'City updated!'], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/deleteCity/{idCity}", name="deleteCity", methods={"DELETE"})
+     */
+    public function deleteCity($idCity): JsonResponse
+    {
+        $city = $this->cityRepository->findOneBy(['idCity' => $idCity]);
+
+        $this->cityRepository->removeCity($city);
+
+        return new JsonResponse(['status' => 'city deleted!'], Response::HTTP_OK);
     }
 }
--- API/src/Repository/CityRepository.php
+++ API/src/Repository/CityRepository.php
@@ -15,10 +15,58 @@ use Doctrine\ORM\EntityManagerInterface;
  */
 class CityRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $manager)
     {
         parent::__construct($registry, City::class);
+        $this->manager = $manager;
     }
+
+    public function saveCity($name, $weather, $size, $nativeLanguage, $continent, $nativeLanguageDificulty, $costOfLiving, $role, $img, $country, $video)
+    {
+        $nuevoCity = new City();
+
+        $nuevoCity->setName($name);
+        $nuevoCity->setWeaher($weather);
+        $nuevoCity->setSize($size);
+        $nuevoCity->setNativeLanguage($nativeLanguage);
+        $nuevoCity->setContinent($continent);
+        $nuevoCity->setNativeLanguaeDifficulty($nativeLanguageDificulty);
+        $nuevoCity->setCostOfLiving($costOfLiving);
+        $nuevoCity->setRole($role);
+        $nuevoCity->setImg($img);
+        $nuevoCity->setCountry($country);
+        $nuevoCity->setVideo($video);
+
+        $this->manager->persist($nuevoCity);
+        $this->manager->flush();
+    }
+
+    public function updateCity(City $city): City
+    {
+        $this->manager->persist($city);
+        $this->manager->flush();
+
+        return $city;
+    }
+
+    public function removeCity(City $city)
+    {
+        $this->manager->remove($city);
+        $this->manager->flush();
+    }
+    
+    /*
+    public function getCitiesConTiempoEstimadoMenor($tiempo)
+    {
+        $qb = $this->manager->createQueryBuilder();
+
+        $qb->select('p')
+            ->from(City::class, 'p')
+            ->where('p.tiempoEstimado < :tiempo')
+            ->setParameter('tiempo', $tiempo);
+        return $qb->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+    }
+    */
 
     // /**
     //  * @return City[] Returns an array of City objects
