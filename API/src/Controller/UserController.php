f9fbaaf9a4bf610f3b0eaba13c93299d86c00ec0
ruben <rubenmartinezlozano@gmail.com>
2021-02-09 09:35:40 +0100

    Use añadidos y creada función SaveUser

--- API/src/Controller/UserController.php
+++ API/src/Controller/UserController.php
@@ -2,20 +2,130 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index(): Response
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/addUser", name="addUser", methods={"POST"})
+     */
+    public function addUser(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $nombre = $data['nombre'];
+        $descripcion = $data['descripcion'];
+        $imagen = $data['imagen'];
+        $tiempoEstimado = $data['tiempo_estimado'];
+
+        if (empty($nombre) || empty($descripcion) || empty($imagen) || empty($tiempoEstimado)) {
+            throw new NotFoundHttpException('Expecting mandatory parameters!');
+        }
+
+        $this->userRepository->saveUser($nombre, $descripcion, $imagen, $tiempoEstimado);
+
+        return new JsonResponse(['status' => 'User Created'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/getUser/{id}", name="getUser", methods={"GET"})
+     */
+    public function getUser($id): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['id' => $id]);
+
+        $data = [
+            'id' => $user->getId(),
+            'nombre' => $user->getNombre(),
+            'descripcion' => $user->getDescripcion(),
+            'imagen' => $user->getImagen(),
+            'tiempo_estimado' => $user->getTiempoEstimado()
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getUsers", name="getUsers", methods={"GET"})
+     */
+    public function getUsers(): JsonResponse
+    {
+        $users = $this->userRepository->findAll();
+
+        $data = [];
+
+        foreach ($users as $p) {
+            $data[] = [
+                'id' => $p->getId(),
+                'nombre' => $p->getNombre(),
+                'descripcion' => $p->getDescripcion(),
+                'imagen' => $p->getImagen(),
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getUsersMenorTiempo/{tiempo}", name="getUsersMenorTiempo", methods={"GET"})
+     */
+    public function getUsersConTiempoEstimadoMenor($tiempo): JsonResponse
+    {
+        $data = $this->userRepository->getUsersConTiempoEstimadoMenor($tiempo);
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getUsersConCategoria/{id}", name="getUsersConCategoria", methods={"GET"})
+     */
+    public function getUsersConCategoria($id): JsonResponse
+    {
+        $data = $this->userRepository->getUsersCategoria($id);
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/updateUser/{id}", name="updateUser", methods={"PUT"})
+     */
+    public function editUser(Request $request, $id): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['id' => $id]);
+        $data = json_decode($request->getContent(), true);
+
+        empty($data['nombre']) ? true : $user->setNombre($data['nombre']);
+        empty($data['descripcion']) ? true : $user->setDescripcion($data['descripcion']);
+        empty($data['imagen']) ? true : $user->setImagen($data['imagen']);
+        empty($data['tiempo_estimado']) ? true : $user->setTiempoEstimado($data['tiempo_estimado']);
+
+        $updatedUser = $this->userRepository->updateUser($user);
+
+        return new JsonResponse(['status' => 'User updated!'], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/deleteUser/{id}", name="deleteUser", methods={"DELETE"})
+     */
+    public function deleteUser($id): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['id' => $id]);
+
+        $this->userRepository->removeUser($user);
+
+        return new JsonResponse(['status' => 'user deleted!'], Response::HTTP_OK);
     }
 }
--- API/src/Repository/UserRepository.php
+++ API/src/Repository/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,9 +15,67 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class UserRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $manager)
     {
         parent::__construct($registry, User::class);
+        $this->manager = $manager;
+    }
+
+    public function saveUser($role, $nickname, $pass, $email, $number, $favouriteWeater, $mainLenguage, $english, $continent, $country)
+    {
+        $nuevoUser = new User();
+
+        $nuevoUser->SetRole($role);
+        $nuevoUser->SetNickname($nickname);
+        $nuevoUser->SetPass($pass);
+        $nuevoUser->SetEmail($email);
+        $nuevoUser->SetNumber($number);
+        $nuevoUser->SetFavouriteWeather($favouriteWeater);
+        $nuevoUser->setMainLanguage($mainLenguage);
+        $nuevoUser->setEnglish($english);
+        $nuevoUser->setContinent($continent);
+        $nuevoUser->setCountry($country);
+
+        $this->manager->persist($nuevoUser);
+        $this->manager->flush();
+    }
+
+    public function updateUser(User $user): User
+    {
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        return $user;
+    }
+
+    public function removeUser(User $user)
+    {
+        $this->manager->remove($user);
+        $this->manager->flush();
+    }
+
+    public function getUsersCategoria($id)
+    {
+        //$em = $this->getDoctrine()->getManager();
+        //$em = $this->getRepository('AppBundle:User');
+        $qb = $this->manager->createQueryBuilder();
+
+        $qb->select('p')
+            ->from(User::class, 'p')
+            ->where('p.categoria_id = :categ')
+            ->setParameter('categ', $id);
+        return $qb->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+    }
+
+    public function getUsersConTiempoEstimadoMenor($tiempo)
+    {
+        $qb = $this->manager->createQueryBuilder();
+
+        $qb->select('p')
+            ->from(User::class, 'p')
+            ->where('p.tiempoEstimado < :tiempo')
+            ->setParameter('tiempo', $tiempo);
+        return $qb->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
     }
 
     // /**
